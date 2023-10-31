e6eff51c3cd0c6bb25b52403b1f4433e3be3804a
DeshiHacker <manis.programmer@gmail.com>
2023-10-31 08:51:45 -0400

    Add files via upload

---
+++ love.py
@@ -0,0 +1,11 @@
+BoyName = input("please enter male name: ")
+GirlName = input("enter female name please: ")
+Total = len(BoyName) + len(GirlName)
+print(Total)
+
+if  8 >= Total <=10:
+    print("Your love is 50% +++")
+elif   10 >= Total <=12:
+    print("Your Relationship is perfect.")
+else:
+    print("Your relationship is in risky zone. be careful!")
